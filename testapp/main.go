05ad2621418c80ec77fec572ef422fba6e0c4371
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-10-23 13:25:47 +0530

    Removing iris

--- testapp/main.go
+++ testapp/main.go
@@ -3,35 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/kataras/iris/v12"
 	"io"
 	"net/http"
 )
 
-type AppContext struct {
-	app *iris.Application
-}
-
-func (a AppContext) NewAppContext() *AppContext {
-	return &AppContext{
-		app: iris.New(),
-	}
-}
-
 func main() {
-	appCtx := AppContext{}
-	app := appCtx.NewAppContext().app
+	http.HandleFunc("/books", list)
+	http.HandleFunc("/books/from", getOtherBooks)
 
-	booksAPI := app.Party("/books")
-	{
-		booksAPI.Use(iris.Compression)
-
-		// GET: http://localhost:8080/books
-		booksAPI.Get("/", appCtx.list)
-		booksAPI.Get("/from/{target}", appCtx.getOtherBooks)
+	// Start the HTTP server.
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Failed to start the server: %s\n", err)
 	}
-
-	app.Run(iris.Addr(":8080"))
 }
 
 // Book example.
@@ -39,38 +22,37 @@ type Book struct {
 	Title string `json:"title"`
 }
 
-func (a AppContext) getOtherBooks(ctx iris.Context) {
+func getOtherBooks(w http.ResponseWriter, r *http.Request) {
 	println("getOtherBooks")
-	var p struct {
-		Target string `json:"target"`
+	target := r.URL.Query().Get("target")
+	if target == "" {
+		http.Error(w, "Target URL not specified", http.StatusBadRequest)
+		return
 	}
-	ctx.ReadParams(&p)
-
-	target := p.Target
 	println(target)
 	url := "http://" + target + "/books" // Replace with the actual URL.
 
 	// Make the HTTP GET request.
 	response, err := http.Get(url)
 	if err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.WriteString(fmt.Sprintf("Failed to make the HTTP GET request: %s", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		//ctx.WriteString(fmt.Sprintf("Failed to make the HTTP GET request: %s", err.Error()))
 		return
 	}
 	defer response.Body.Close()
 
 	// Check if the response status code is OK (200).
 	if response.StatusCode != http.StatusOK {
-		ctx.StatusCode(http.StatusBadGateway)
-		ctx.WriteString(fmt.Sprintf("HTTP GET request failed with status code: %d", response.StatusCode))
+		w.WriteHeader(http.StatusBadGateway)
+		//ctx.WriteString(fmt.Sprintf("HTTP GET request failed with status code: %d", response.StatusCode))
 		return
 	}
 
 	// Read the response body.
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.WriteString(fmt.Sprintf("Failed to read response body: %s", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		//ctx.WriteString(fmt.Sprintf("Failed to read response body: %s", err.Error()))
 		return
 	}
 
@@ -78,25 +60,27 @@ func (a AppContext) getOtherBooks(ctx iris.Context) {
 	var books []Book
 	err = json.Unmarshal(body, &books)
 	if err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.WriteString(fmt.Sprintf("Failed to unmarshal JSON payload: %s", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		//w.WriteString(fmt.Sprintf("Failed to unmarshal JSON payload: %s", err.Error()))
 		return
 	}
 	books = append(books, Book{"Go Go Go"})
 	// Send the response body to the client.
-	resp, err := json.Marshal(books)
-
-	ctx.StatusCode(http.StatusOK)
-	ctx.Write(resp)
+	//resp, err := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(books)
+	//ctx.Write(resp)
 }
 
-func (a AppContext) list(ctx iris.Context) {
+func list(w http.ResponseWriter, r *http.Request) {
 	println("list")
 	books := []Book{
 		{"Mastering Concurrency in Go"},
 		{"Go Design Patterns"},
 		{"Black Hat Go"},
 	}
-	ctx.StatusCode(http.StatusOK)
-	ctx.JSON(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(books)
 }
